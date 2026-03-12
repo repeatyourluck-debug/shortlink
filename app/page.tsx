9e72bd15d75b21302ebe0961df3d89aa773b4b86
repeatyourluck-debug <repeatyourluck@gmail.com>
2026-03-13 00:28:38 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Smart Redirector</h1>
+      <p>System is online.</p>
+    </main>
+  );
+}
